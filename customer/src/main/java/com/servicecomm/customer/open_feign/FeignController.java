fcedc4908a95f1e8e74e0545c747f236000fd708
arghya <royarghyadip6@gmail.com>
2026-06-20 00:11:30 +0530

    InterServiceComm: Using OpenFeign, inter-service communication

--- customer/src/main/java/com/servicecomm/customer/open_feign/FeignController.java
+++ customer/src/main/java/com/servicecomm/customer/open_feign/FeignController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeignController {
     private final OpenFeignClient openFeignClient;
 
+    /**
+     * OpenFeign is used to communicate with microservices. If new API need to be accessed from provider microservice, just add a new method in OpenFeignClient interface and call it here.
+     * OpenFeign splits the communication layer cleanly between an Interface (which defines how to talk to the remote service) and a Controller (which handles incoming requests and consumes that interface).
+     * @return
+     */
     @RequestMapping("/provider-info")
     public String getProviderInfo() {
         return openFeignClient.getInstance();
