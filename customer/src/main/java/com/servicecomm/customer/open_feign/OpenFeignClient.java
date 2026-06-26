11e6de55a91f438a178d45000488419571e6d62d
arghya <royarghyadip6@gmail.com>
2026-06-26 20:50:45 +0530

    InterServiceComm: Using @LoadBalanced with OpenFeign for Declarative API Calls
    Signed-off-by: Arghyadip Roy (EXT-Nokia) <arghyadip.roy.ext@nokia.com>

--- customer/src/main/java/com/servicecomm/customer/open_feign/OpenFeignClient.java
+++ customer/src/main/java/com/servicecomm/customer/open_feign/OpenFeignClient.java
@@ -3,6 +3,16 @@ package com.servicecomm.customer.open_feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Feign client for calling the provider service.
+ *
+ * Note: You do not annotate a Feign client with @LoadBalanced. When using
+ * Spring Cloud and service discovery (Eureka) or Spring Cloud LoadBalancer,
+ * Feign resolves the logical service name (the value of {@code name}) and
+ * will perform client-side load balancing for you. If you need a
+ * LoadBalanced RestTemplate, annotate the RestTemplate bean with
+ * {@code @LoadBalanced} instead.
+ */
 //@FeignClient(name = "provider-service", url = "http://localhost:8081")
 @FeignClient(name = "provider")
 public interface OpenFeignClient {
